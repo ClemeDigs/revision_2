fd6e29d818869ef1a89d440fbceffc69e1a6e246
Clementine Digout <clementine.digout@yahoo.fr>
2024-09-24 19:49:10 -0400

    add types

--- inputToOutput.js
+++ inputToOutput.js
@@ -1,13 +1,36 @@
+/**
+ * @param {object} input
+ * @returns {string}
+ */
 export function inputToOutput(input) {
+    /**
+     * @type {Array<[string, string | number]>}
+     */
     const entries = Object.entries(input); 
+    /**
+     * @type {string}
+     */
     let resultString = '!!%' + input.type + '%'; 
 
+
     for (let i = 1; i < entries.length; i++) {
+        /**
+         * @type {string}
+         */
         let key = entries[i][0]; 
+        /**
+         * @type {string | number}
+         */
         let value = entries[i][1]; 
+        /**
+         * @type {string}
+         */
         let modifiedKey = ''; 
         
         for (let j = 0; j < key.length; j++) {
+            /**
+             * @type {string}
+             */
             let char = key[j]; 
 
             if (char === char.toUpperCase() && char !== char.toLowerCase()) {
@@ -17,21 +40,15 @@ export function inputToOutput(input) {
             }
         }
 
+        /**
+         * @type {string}
+         */
         let keysString = `%attribute-${modifiedKey}="${value}"%`; 
         resultString += keysString;
     }
 
     resultString += '!!'; 
     resultString = resultString.replace(',', '&');
-    console.log(resultString);
 
     return resultString; 
 }
-
-
-
-
-
-
-
-
